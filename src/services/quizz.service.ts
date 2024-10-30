19c9fafd7548c1e58bf1a7e9c6c73eaf8029ab49
vogiaan1904 <vogiaan1904@gmail.com>
2024-10-30 12:27:24 +0700

    test cicd

--- src/services/quizz.service.ts
+++ src/services/quizz.service.ts
@@ -48,6 +48,7 @@ class QuizzService {
     const position = numQuizzes.length;
 
     const quizz = await quizzRepo.create(data, position);
+    console.log("quizz created successfully");
     return quizz;
   };
 
