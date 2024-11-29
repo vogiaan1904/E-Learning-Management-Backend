210f5a7a36be957f10a3bd92ec8ae2b4180fb708
vogiaan1904 <vogiaan1904@gmail.com>
2024-11-29 17:56:09 +0700

    update notification

--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,33 +1,33 @@
-// import { routesConfig } from "@/configs";
-// import notificationController from "@/controllers/notification.controller";
-// import { Request, Response, Router } from "express";
-// import { StatusCodes } from "http-status-codes";
+import { routesConfig } from "@/configs";
+import notificationController from "@/controllers/notification.controller";
+import { Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
-// const router = Router({ mergeParams: true });
-// const { notificationRoute } = routesConfig;
+const router = Router({ mergeParams: true });
+const { notificationRoute } = routesConfig;
 
-// router.get(notificationRoute.status, (req: Request, res: Response) => {
-//   res.status(StatusCodes.OK).json({
-//     message: "Notification APIs",
-//     status: "success",
-//   });
-// });
+router.get(notificationRoute.status, (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({
+    message: "Notification APIs",
+    status: "success",
+  });
+});
 
-// router.post(
-//   notificationRoute.sendNotification,
-//   notificationController.sendNotification,
-// );
+router.post(
+  notificationRoute.sendNotification,
+  notificationController.sendNotification,
+);
 
-// router.post(notificationRoute.sendFCMToken, notificationController.sendToken);
+router.post(notificationRoute.sendFCMToken, notificationController.sendToken);
 
-// router.get(
-//   notificationRoute.getNotification,
-//   notificationController.getNotification,
-// );
+router.get(
+  notificationRoute.getNotification,
+  notificationController.getNotification,
+);
 
-// router.get(
-//   notificationRoute.getNotifications,
-//   notificationController.getNotifications,
-// );
+router.get(
+  notificationRoute.getNotifications,
+  notificationController.getNotifications,
+);
 
-// export const notificationApis = router;
+export const notificationApis = router;
--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,6 +1,6 @@
-import firebaseAdmin from "@/configs/firebase.config";
 import { TokenMessage } from "firebase-admin/lib/messaging/messaging-api";
 
+import firebaseAdmin from "@/configs/firebase.config";
 const messaging = firebaseAdmin.messaging();
 
 export const sendToOneToken = async (
